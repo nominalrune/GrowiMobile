225c887ef239eea420c5d4205af32c4df702747c
nominalrune <59037577+nominalrune@users.noreply.github.com>
2024-08-24 08:53:00 +0900

    change layout

--- app/ _layout.tsx
+++ app/ _layout.tsx
@@ -7,16 +7,27 @@ import { router } from 'expo-router';
 import { Slot } from 'expo-router';
 import useAuthStore from '../hooks/useAuthStore';
 import GrowiAPI from '../services/GrowiAPI/GrowiAPI';
+import { Stack } from 'expo-router/stack';
 
-export default function Layout({ children }: { children: ReactNode; }) {
-
-	return <ApiProvider>
-		<SafeAreaView className='h-full'>
-			{children}
-			{/* <Slot /> */}
-		</SafeAreaView>
-	</ApiProvider>;
+export default function Layout() {
+  return (
+	<ApiProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+	</ApiProvider>
+  );
 }
+
+// export default function Layout({ children }: { children: ReactNode; }) {
+
+// 	return <ApiProvider>
+// 		<SafeAreaView className='h-full'>
+// 			{children}
+// 			{/* <Slot /> */}
+// 		</SafeAreaView>
+// 	</ApiProvider>;
+// }
 
 // function LoginOnlyContent({ children }: { children: ReactNode; }) {
 // 	return api
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,7 @@
+import { Link, router } from 'expo-router';
+import { View, Text, Pressable, SafeAreaView } from 'react-native';
+import Home from '../../components/Home';
+
+export default function Index() {
+	return <Home/>;
+}
