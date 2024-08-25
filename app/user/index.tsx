17b595232f9ff709c85adb2b680422bbfe80adc7
yoshinori yokoyama <nominalrune.yokoyama@gmail.com>
2024-08-25 14:36:17 +0900

    add package

--- app/user/index.tsx
+++ app/user/index.tsx
@@ -8,19 +8,25 @@ import useUser from '../../hooks/useUser';
 export default function User() {
 	const { get } = useAuthStore();
 	const [s, setS] = useState<{ url: string, token: string; }>();
-	const { user } = useUser();
+	const { user, fetchUser } = useUser();
 	useEffect(() => {
 		get().then(r => {
 			setS(r);
 		});
 	}, []);
+	async function refreshProfile() {
+		const result = await fetchUser();
+		console.log('fetched user', result);
+	}
 	return <View className='flex m-2 mx-4 '>
 		{user ? <>
 			<Text className='text-lg'>{user.name}</Text>
-			<Image className='m-2 border shadow' source={{ uri: user.imageUrlCached }} alt='user_icon' />
+			<Image className='w-24 h-24 border shadow bg-white' source={{ uri: user.imageUrlCached }} alt='user_icon' />
+			<Image className='w-24 h-24 bg-gray-400' src='data:image/png;UTF-8,https://wiki.paulsenglish.jp/attachment/66bd7c4a5a9c2c8eef0f0ffc' />
 		</> : <Text>loading</Text>}
 		<Text>url: <Link className='text-blue-700 underline' href={s?.url ?? ''}>{s?.url}</Link></Text>
 		<Text>token: {s?.token}</Text>
 		<Button onPress={() => router.push('user/login')}><Text>login</Text></Button>
+		<Button onPress={() => refreshProfile()}><Text>refresh profile</Text></Button>
 	</View>;
 }
--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -11,23 +11,24 @@ export default function useUser() {
 	useEffect(() => {
 		(async () => {
 			const user = await storage.getObject<IUser>('user');
-			console.log('user in store',user)
+			console.log('user in store', user);
 			if (user) {
 				setUser(user);
 			} else {
 				fetchUser();
 			}
 		})();
-		const i = setInterval(()=>{console.log(Date(),'user',user)},100_000)
-		return ()=>{clearInterval(i) }
+		const i = setInterval(() => { console.log(Date(), 'user', user); }, 100_000);
+		return () => { clearInterval(i); };
 	}, []);
 	async function fetchUser() {
 		if (!api) throw new Error('api no set');
-		const url = await storage.get('url');
+		const url = (await storage.get('url')) ?? '';
 		const _user = (await api.fetchUser()).currentUser;
-		const user = { ..._user, imageUrlCached: url + _user.imageUrlCached, apiToken: undefined };
+		const user = { ..._user, imageUrlCached: url.replace(/\/$/, '') + _user.imageUrlCached, apiToken: undefined };
 		await storage.set('user', user);
 		setUser(user);
+		return user;
 	}
 	return { user, fetchUser };
 }
