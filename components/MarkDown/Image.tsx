1c468c4528d671506bb1c6bbab059745f05acaa2
nominalrune <59037577+nominalrune@users.noreply.github.com>
2024-09-06 15:45:53 +0900

    pkgupdate

--- components/MarkDown/Image.tsx
+++ components/MarkDown/Image.tsx
@@ -1,11 +1,16 @@
 import { Tokens } from 'marked';
 import { Image as ImageType } from "mdast";
-import { Text, View, Image } from 'react-native';
-export default function({ node }: { node: Tokens.Image; }) {
-	return <View>
-		<Text className='underline'>{node.href}</Text>
-		<Image width={20} height={30} style={{backgroundColor:"blue"}} src={node.href} source={{uri:node.href}} alt={node.text}/>
-		<Image style={{backgroundColor:"blue"}} src={'https://fastly.picsum.photos/id/1068/200/300.jpg?hmac=ICIwYXRGTpDxBPZAI7V8YxGtBBanV8Dfbe_DLNKtYcE'} source={{uri:node.href}} alt={node.text}/>
-		<Text className='underline'>{node.href}</Text>
+import { Text, View } from 'react-native';
+import { Image } from 'expo-image';
+export default function ({ node }: { node: Tokens.Image; }) {
+	return <View style={{
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	}}><Image style={{
+		
+		width: 100,
+		height:100,
+	}} contentFit="none" source={'https://fastly.picsum.photos/id/1068/200/300.jpg?hmac=ICIwYXRGTpDxBPZAI7V8YxGtBBanV8Dfbe_DLNKtYcE'} alt={node.text} />
 	</View>;
 }
